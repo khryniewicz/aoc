4879bec084a056ca490c0baafc977d199f9b7c2d
Kamil Hryniewicz <kamil.hryniewicz@gmail.com>
2020-12-15 22:16:03 +0100

    Day 15

---
+++ src/day15.clj
@@ -0,0 +1,15 @@
+(ns day15)
+
+(def input [6 13 1 15 2 0])
+
+(defn memory-game [starting end]
+  (loop [turn (count starting)
+         recent (last starting)
+         memory (zipmap (butlast starting) (range 1 (count starting)))]
+    (if (= turn end)
+      recent
+      (recur (inc turn) (- turn (memory recent turn)) (assoc memory recent turn)))))
+
+(def answer1 (memory-game input 2020))
+
+(def answer2 (memory-game input 30000000))
